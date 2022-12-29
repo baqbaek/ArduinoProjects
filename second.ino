ec3851ec84078d4ce97289d142e64dc04f6881fe
Bartosz Kubiczek <47948027+baqbaek@users.noreply.github.com>
2022-12-29 20:52:20 +0100

    Update second.ino

--- second.ino
+++ second.ino
@@ -13,24 +13,22 @@ void setup() //DEKLARACJA UZYWANYCH PINOW
  pinMode(8, OUTPUT);
  pinMode(9, OUTPUT);
  pinMode(10, OUTPUT);
-//USTAWIANIE WARTOSCI POCZATKOWYCH
-ZM.
- zm1 = 250;
- zm2 = 1;
- zm3 = 1;
+//USTAWIANIE WARTOSCI POCZATKOWYCH ZM.
+  zm1 = 250;
+  zm2 = 1;
+  zm3 = 1;
 }
 void loop()
 {
 
  for (zm3 = 0; zm3 < 10; zm3++){ //PETLA POWTARZAJACA SIE 10X
- for (zm2 = 1; zm2 < 11; zm2++){ //PETLA ZALACZAJACE LAMPKI W KOLEJNOSCI
- for(int i = 0; i < zm3; i++){ //PETLA ODPOWIADAJCA ZA ILOSC POWTORZEN WLACZENIA DIODY
- digitalWrite(zm2, HIGH);
- delay(zm1); //OPOZNIENIE
- digitalWrite(zm2, LOW);
- delay(zm1);
- }
- }
-
+     for (zm2 = 1; zm2 < 11; zm2++){ //PETLA ZALACZAJACE LAMPKI W KOLEJNOSCI
+         for(int i = 0; i < zm3; i++){ //PETLA ODPOWIADAJCA ZA ILOSC POWTORZEN WLACZENIA DIODY
+              digitalWrite(zm2, HIGH);
+              delay(zm1); //OPOZNIENIE
+              digitalWrite(zm2, LOW);
+              delay(zm1);
+         }
+     }
  }
 }
